e69b7ec6139ab0752f4ec1c339c05057b31f5b55
Harry Barber <harrybarber@protonmail.com>
2020-04-13 23:26:37 +0100

    Add UTXO ID calculation from parts method

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -2,6 +2,10 @@ export const calcId = function (output) {
   return output.txId.slice(0, 20) + output.outputIndex
 }
 
+export const caldIdFromParts = function (txId, vout) {
+  return txId.slice(0, 20) + vout
+}
+
 export const stampPrice = function (outpoints) {
   if (outpoints !== null) {
     let amount = outpoints.reduce((a, stampOutpoint) => {
